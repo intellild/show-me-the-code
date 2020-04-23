23483b288c76e146ef02dfaf690596f189125b58
intellild <intellild@outlook.com>
2020-04-23 23:20:22 +0800

    update

--- assets/src/services/editor.service.ts
+++ assets/src/services/editor.service.ts
@@ -1,19 +1,9 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import * as monaco from 'monaco-editor';
-import { BehaviorSubject, Subscription, Subject } from 'rxjs';
-
-// const defaultTypeScriptCompilerOption: monaco.languages.typescript.CompilerOptions = {
-//   target: monaco.languages.typescript.ScriptTarget.ES2018,
-//   strictNullChecks: true,
-//   strictFunctionTypes: true,
-//   lib: ['es2018'],
-//   experimentalDecorators: false,
-//   emitDecoratorMetadata: false,
-//   jsx: monaco.languages.typescript.JsxEmit.None,
-// };
+import { BehaviorSubject, Subject, Subscription } from 'rxjs';
+import { distinctUntilChanged } from 'rxjs/operators';
 
 const THEME_KEY = 'THEME';
-// monaco.languages.typescript.typescriptDefaults.setCompilerOptions(defaultTypeScriptCompilerOption);
 
 @Injectable()
 export class EditorService implements OnDestroy {
@@ -25,28 +15,28 @@ export class EditorService implements OnDestroy {
   expires: Date | null = null;
 
   readonly model = monaco.editor.createModel('', this.language$.getValue());
-  private $$: Subscription[] = [];
+  private $: Subscription[] = [];
 
   constructor() {
     const initialTheme = localStorage.getItem(THEME_KEY) || 'vs-dark';
     this.theme$ = new BehaviorSubject(initialTheme);
-    this.$$.push(
-      this.language$.subscribe(language => {
+    this.$.push(
+      this.language$.pipe(distinctUntilChanged()).subscribe((language) => {
         monaco.editor.setModelLanguage(this.model, language);
       }),
-      this.theme$.subscribe(theme => {
+      this.theme$.subscribe((theme) => {
         localStorage.setItem(THEME_KEY, theme);
         monaco.editor.setTheme(theme);
       }),
     );
   }
 
-  async format() {
+  format() {
     this.format$.next();
   }
 
   ngOnDestroy() {
-    this.$$.forEach(it => it.unsubscribe());
-    this.$$ = [];
+    this.$.forEach((it) => it.unsubscribe());
+    this.$ = [];
   }
 }
